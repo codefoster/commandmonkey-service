5efe75e6f85ba7e7911ac756f65f3d574be7859b
Jeremy Foster <jeremy.foster@live.com>
2015-10-15 16:27:04 -0700

    add console logging

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ var targetSocket;
 //setup the http route (for receiving commands)
 app.get('/api/command', function (req, res) {
     //if there's a target, send the command via a socket message
-    if (targetSocket) targetSocket.emit('command', req.query.cmd);
+    if (targetSocket) {
+        targetSocket.emit('command', req.query.cmd);
+        console.log(req.query.cmd + ' command received and sent to target device');
+    }
+    else {
+        console.log('command received but no devices have checked in yet to receive the command (' + req.query.cmd + ')');
+    }
 });
 
 //setup the sockets (for setting target)
